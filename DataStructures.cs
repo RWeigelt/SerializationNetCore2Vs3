812f8d2323ddff70c621439afc702f37805d65aa
Roland Weigelt <mail@roland-weigelt.de>
2019-10-18 19:36:31 +0200

    Add project files.

---
+++ DataStructures.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerializationNetCore2Vs3
+{
+	public class Thing
+	{
+		public Thing()
+		{
+			Items=new List<Item>();
+		}
+
+		public List<Item> Items { get; set; }
+	}
+
+	public class Item
+	{
+		public Item()
+		{
+			SubItems = new List<SubItem>();
+		}
+
+		public List<SubItem> SubItems { get; }
+	}
+
+	public class SubItem
+	{
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SerializationNetCore2Vs3
+{
+	class Program
+	{
+		private static Item CreateItem()
+		{
+			var item = new Item();
+			item.SubItems.Add(new SubItem());
+			item.SubItems.Add(new SubItem());
+			item.SubItems.Add(new SubItem());
+			item.SubItems.Add(new SubItem());
+			return item;
+		}
+		static void Main(string[] args)
+		{
+			var original = new Thing();
+			original.Items.Add(CreateItem());
+			original.Items.Add(CreateItem());
+
+
+			var json = System.Text.Json.JsonSerializer.Serialize(original);
+			var json2 = Newtonsoft.Json.JsonConvert.SerializeObject(original);
+			Console.WriteLine($"JSON is equal: {String.Equals(json, json2, StringComparison.Ordinal)}");
+			Console.WriteLine();
+
+			var instance1 = System.Text.Json.JsonSerializer.Deserialize<Thing>(json);
+			var instance2 = Newtonsoft.Json.JsonConvert.DeserializeObject<Thing>(json);
+
+			Console.WriteLine($".Items.Count: {instance1.Items.Count} (System.Text.Json)");
+			Console.WriteLine($".Items.Count: {instance2.Items.Count} (Json.NET)");
+			Console.WriteLine();
+			Console.WriteLine($".Items[0].SubItems.Count: {instance1.Items[0].SubItems.Count} (System.Text.Json)");
+			Console.WriteLine($".Items[0].SubItems.Count: {instance2.Items[0].SubItems.Count} (Json.NET)");
+		}
+	}
+}
